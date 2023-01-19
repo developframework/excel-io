fd769918cc2ce27b7fa8e7ef7f939c5391d2070f
qiushui <qiushui@zgpgc.com>
2023-01-19 17:40:27 +0800

    预处理

---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+/**
+ * @author qiushui on 2023-01-19.
+ */
+module excel.io {
+    requires expression;
+    requires lombok;
+    requires org.apache.commons.lang3;
+    requires poi;
+    requires poi.ooxml;
+    requires org.slf4j;
+
+    exports com.github.developframework.excel.column;
+    exports com.github.developframework.excel.styles;
+    exports com.github.developframework.excel;
+
+}
